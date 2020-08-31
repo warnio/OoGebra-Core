829063a01ccd2573c586fe29ce9a66cfc56cacef
WarNIO <me.warnio@gmail.com>
2020-08-31 11:57:39 +0200

    Rename index.ts to load.ts

--- src/load.ts
+++ src/load.ts
@@ -1,11 +1,9 @@
 
-namespace OoGebra { }
-
-OoGebra.init();
-
 declare const global: any;
 if (!global.hasOwnProperty('OoGebra')) {
   Object.defineProperty(global, 'OoGebra', {
     value: OoGebra
   });
 }
+
+OoGebra.init();
